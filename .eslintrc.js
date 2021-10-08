43822e54d45d540e3f7f72453bd4508894e80eab
Arsalan Khattak <me@khattak.dev>
2021-10-08 16:59:04 +0500

    add pre-commit test

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,23 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['airbnb-base', 'prettier'],
+  extends: ['airbnb-base', 'prettier', 'plugin:react/recommended'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
   rules: {
     'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'import/no-unresolved': 'warn',
   },
   plugins: ['prettier'],
 };
