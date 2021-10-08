64f879e65485742e634e204ec8f210cf9f7ecc1f
Arsalan Khattak <me@khattak.dev>
2021-10-08 12:31:35 +0500

    added scripts in package.json

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,16 @@
-console.log("Backend Service!!!");
+const fastify = require('fastify')({
+  logger: true,
+});
+
+fastify.get('/', async (request, reply) => {
+  reply.type('application/json').code(200);
+  return { hello: 'world' };
+});
+
+fastify.listen(3001, (err, address) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  fastify.log.info(`Server started at ${address}`);
+});
